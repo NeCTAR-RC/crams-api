160c3887f6dc43433874aa9f4a18fe52ec03ae85
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-04 17:07:38 +1000

    Setup Versioning  - Version 1  --> v1  - Version 2  --> v2 (empty)

--- crams_app/settings.py
+++ crams_app/settings.py
@@ -139,9 +139,9 @@ REST_FRAMEWORK = {
     ),
     # Other
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
-    'ALLOWED_VERSIONS': ['v1', 'v2'],
-    'DEFAULT_VERSION': 'v1',
+    # 'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+    # 'ALLOWED_VERSIONS': ['v1', 'v2'],
+    # 'DEFAULT_VERSION': 'v1',
     # Pagination Fields
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 2,
