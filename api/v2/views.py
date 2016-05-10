effc51d353d3a5fc222b0524e261efd414ee45b7
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-10 14:28:24 +1000

    Remove validation for Storage Product quota/product id during approval process

--- api/v2/views.py
+++ api/v2/views.py
@@ -1,7 +1,6 @@
-__author__ = 'rafi m feroze'  # 'mmohamed'
-
 from django.http import HttpResponse
 
+__author__ = 'rafi m feroze'  # 'mmohamed'
 
 def not_implemented(request):
     return HttpResponse('<H3>Not implemented</H3>')
