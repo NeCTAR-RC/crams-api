4468489e6b425963473354409da0f6a9fb6b6fa9
Sam Morrison <sorrison@gmail.com>
2016-05-13 10:58:40 +1000

    Fix up requirements
    Change-Id: I2c6c6dd4c5489803cea2624a0f6142e507e5976d

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
+
+requirements = parse_requirements("requirements.txt", session=False)
 readme = open('README.md').read()
+
 
 setup(
     name='crams',
@@ -18,51 +19,7 @@ setup(
     url='https://github.com/NeCTAR-RC/crams-api',
     packages=find_packages(exclude=['tests', 'local']),
     include_package_data=True,
-    install_requires=[
-                      'astroid==1.4.4',
-                      'Babel==1.3',
-                      'colorama==0.3.6',
-                      'coverage==4.0.3',
-                      'debtcollector==1.1.0',
-                      'Django==1.8.7',
-                      'django-babel==0.3.9',
-                      'django-cors-headers==1.1.0',
-                      'django-extensions==1.6.1',
-                      'django-filter==0.9.2',
-                      'djangorestframework==3.2.3',
-                      'flake8==2.5.1',
-                      'gunicorn==19.3.0',
-                      'iso8601==0.1.11',
-                      'keystoneauth1==2.1.0',
-                      'mccabe==0.3.1',
-                      'monotonic==0.5',
-                      'msgpack-python==0.4.6',
-                      'netaddr==0.7.18',
-                      'netifaces==0.10.4',
-                      'numpy==1.9.2',
-                      'oslo.config==3.2.0',
-                      'oslo.i18n==3.1.0',
-                      'oslo.serialization==2.2.0',
-                      'oslo.utils==3.3.0',
-                      'pbr==1.8.1',
-                      'pep8==1.6.2',
-                      'pluggy==0.3.1',
-                      'prettytable==0.7.2',
-                      'py==1.4.31',
-                      'pyflakes==1.0.0',
-                      'pylint==1.5.4',
-                      'pyparsing==2.0.3',
-                      'python-dateutil==2.4.2',
-                      'python-keystoneclient==2.0.0',
-                      'pytz==2014.10',
-                      'requests==2.9.1',
-                      'six==1.9.0',
-                      'sqlparse==0.1.18',
-                      'stevedore==1.10.0',
-                      'tox==2.3.1',
-                      'virtualenv==15.0.1',
-                      'wrapt==1.10.6'
-                      ],
+    install_requires=[str(r.req) for r in requirements],
     license="GPLv3+",
     zip_safe=False,
     keywords='crams',
