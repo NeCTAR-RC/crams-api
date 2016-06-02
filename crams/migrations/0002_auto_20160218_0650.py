e210a2dd4fac6915443237b32ee92ec151e3f324
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-06-02 17:23:10 +1000

    Add new model to store migration data  - data that exists in previous system, but not used in CRAMS at migration time Update Provision Status   - New status Resend   - New status Updated Add mysql related internal Flags Fix client return URL after authentication.  - redirect to crams-rc-frontend fails without a '/' at end of URL Update .gitignore  - ignore system specific build and dist files
    Change-Id: I6a8ce65300fca75074251e3d98e49ddfa37eb100

--- crams/migrations/0002_auto_20160218_0650.py
+++ crams/migrations/0002_auto_20160218_0650.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
+# from django.conf import settings
+
 
 def load_crams_inital_data_from_sql():
     from crams.settings import BASE_DIR
-
+    from crams.settings import DB_MYSQL
     import os
 
-    sql_statements = open(os.path.join(BASE_DIR,
-                                       'crams/sql/crams_initial.sql'), 'r').read()
+    file_name = 'crams/sql/crams_initial.sql'
+    # DB_MYSQL = getattr(settings, "DB_MYSQL", False)
+    if DB_MYSQL:
+        file_name = 'crams/sql/crams_initial.mysql.sql'
+
+    sql_statements = open(os.path.join(BASE_DIR, file_name), 'r').read()
+
 
     return sql_statements
 
