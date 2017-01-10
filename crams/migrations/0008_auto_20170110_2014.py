a3e0c30bf5a951b380422ef830f7876b5e073606
Rafi Mohamed Feroze <mohamed.feroze@monash.edu>
2017-01-10 19:47:39 +1100

    Refactor code to update grant information  - make grant_id optional  - add new field duration
    Change-Id: I1217ddfac0f4c2f98defd36b55ac855323a599a5

---
+++ crams/migrations/0008_auto_20170110_2014.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crams', '0007_update_compute_request_min_values'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='grant',
+            name='duration',
+            field=models.IntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(1000)], default=12, error_messages={'max_value': 'Please input a year between 0 ~ 1000', 'min_value': 'Please input a year between 0 ~ 1000'}),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='grant',
+            name='grant_id',
+            field=models.CharField(blank=True, max_length=200, null=True),
+        ),
+        migrations.AlterField(
+            model_name='grant',
+            name='start_year',
+            field=models.IntegerField(validators=[django.core.validators.MinValueValidator(1970), django.core.validators.MaxValueValidator(3000)], default=2017, error_messages={'max_value': 'Please input a year between 1970 ~ 3000', 'min_value': 'Please input a year between 1970 ~ 3000'}),
+        ),
+    ]
